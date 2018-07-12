e55f0160c4dd1033d8bb442cc1ffcf1d3233566a
wellxiao <wellxiao@WellXiao-MacBook-Pro.local>
2018-07-12 09:26:00 +0800

    添加webpack 图片文字xml打包解析

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import _ from 'lodash';
+import _ from "lodash";
+import "./style.css";
+import image from "./image/cat.jpg";
+import Data from "./xml/data.xml";
+
 function component() {
-    var element = document.createElement('div');
-  
-    // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
-    return element;
-  }
-  
-  document.body.appendChild(component());
+  var element = document.createElement("div");
+
+  // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("hello");
+
+  // 将图像添加到我们现有的 div。
+  var myIcon = new Image();
+  myIcon.src = image;
+
+  element.appendChild(myIcon);
+  console.log(Data);
+
+  return element;
+}
+
+document.body.appendChild(component());
